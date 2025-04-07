9b453bcb6e0002f78b1078f8aa180aaac1b419f1
Muhammad Arslan <144650701+imArslanJavaScriptor@users.noreply.github.com>
2025-04-07 06:57:06 +0000

    data types discussiuon

---
+++ 01_Basics/02_dataTypes.js
@@ -0,0 +1,21 @@
+"use strict" // treat all JS code as newer version of JavaScript
+
+// alert("Arslan") // we are using node.js not browser
+//❌console.log( 5 + 5 ) console.log("Arslan");  // code readability should be high
+console.log( 5 + 5 )
+console.log("Arslan"); // ✅
+
+// Data Types in JavaScript 
+// Premetives
+// number => 2 to the power 53
+// bigint 
+// string => ""
+// boolean => true/false
+// null => standalone value null means empty nothing.
+// undefined => value not defined 
+// symbol => unique
+
+// Refrence / Object
+
+console.log(typeof undefined)
+console.log(typeof null)
