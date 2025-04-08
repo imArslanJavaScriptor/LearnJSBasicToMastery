aba9306e3519cb043ba49306b55c1ecc379f5b21
Muhammad Arslan <144650701+imArslanJavaScriptor@users.noreply.github.com>
2025-04-08 04:06:59 +0000

    discussion on operations

--- 01_Basics/03_conversionOperation.js
+++ 01_Basics/03_conversionOperation.js
@@ -23,5 +23,39 @@ let valueInBoolean = Boolean(isLoggedIn)
 
 let numericValue = 200
 let valueInString = String(numericValue)
-console.log(typeof(valueInString))
-console.log(valueInString)
+// console.log(typeof(valueInString))
+// console.log(valueInString)
+
+// ================= Operations ====================
+let val = 3
+let negValue = -val
+// console.log(negValue)
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**4)
+// console.log(2%5)
+
+
+let str1 = "Hi,"
+let str2 = " Arslan"
+let jointString = str1 + str2
+// console.log(jointString)
+
+// console.log("10" + 2)
+// console.log(2 + "10")
+// console.log("1" + 4 + 5)
+// console.log(1 + 4 + "5")
+
+// console.log(+true)
+// console.log(+"")
+
+let val1, val2, val3
+val1 = val2 = val3 = 2 + 8
+// console.table([val1, val2, val3])
+
+let a = 10
+let b = a++
+console.log("a", a)
+console.log("b", b)
