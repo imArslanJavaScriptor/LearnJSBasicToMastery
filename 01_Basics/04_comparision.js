49049d720ce08a2f380bf68da6f60944edc5e6fa
Muhammad Arslan <144650701+imArslanJavaScriptor@users.noreply.github.com>
2025-04-08 05:56:09 +0000

    discussion on comprasion

---
+++ 01_Basics/04_comparision.js
@@ -0,0 +1,22 @@
+// console.log(2 > 1)
+// console.log(2 < 1)
+// console.log(2 >= 1)
+// console.log(2 <= 1)
+
+// console.log("2" > 1)
+// console.log("10" < 20)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)
+// The Reason is that an equality check == and comparisions >,<,>=,<= works diffrenlty
+// comparisions operator convert null to a number 0
+// that's why null >= 0 ==> true 
+
+
+// console.log(undefined == 0)
+// console.log(undefined >= 0)
+
+// === we use triple equal for strict check 
+// yaha pr conversion nhi hoti BCZ yha pr data type bhi check hota ha.
+console.log("10" === 10)
