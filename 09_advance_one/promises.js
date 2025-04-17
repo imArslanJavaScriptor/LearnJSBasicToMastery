4890abaccdcce2b9dd2dcf5a9d126546c233a437
Muhammad Arslan <imarslan7061@gmail.com>
2025-04-18 02:02:03 +0500

    Promises Update

--- 09_advance_one/promises.js
+++ 09_advance_one/promises.js
@@ -1,80 +1,101 @@
-const promiseOne = new Promise(function (resolve, reject) {
-  //Do an async task
-  // DB calls, cryptography, network
-  setTimeout(function () {
-    console.log("Async task is compelete");
-    resolve();
-  }, 1000);
-});
+// const promiseOne = new Promise(function (resolve, reject) {
+//   //Do an async task
+//   // DB calls, cryptography, network
+//   setTimeout(function () {
+//     console.log("Async task is compelete");
+//     resolve();
+//   }, 1000);
+// });
 
-promiseOne.then(function () {
-  console.log("Promise consumed");
-});
+let success = false
+const promise1 = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        if(success) {   
+            console.log("Task is Completed")
+            resolve({name:"Arslan", age: 20, profession: "Software Enginner"})
+        } else {
+            reject("Operation is Failed")
+        }
+    }, 1000);
+})
 
-new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    console.log("Async task 2");
-    resolve();
-  }, 1000);
-}).then(function () {
-  console.log("Async 2 resolved");
-});
+promise1
+.then((data) => {
+    console.log(data)
+})
+.catch((err) => {
+    console.log("Error", err)
+})
 
-const promiseThree = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    resolve({ username: "Chai", email: "chai@example.com" });
-  }, 1000);
-});
 
-promiseThree.then(function (user) {
-  console.log(user);
-});
+// promiseOne.then(function () {
+//   console.log("Promise consumed");
+// });
 
-const promiseFour = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let error = true;
-    if (!error) {
-      resolve({ username: "hitesh", password: "123" });
-    } else {
-      reject("ERROR: Something went wrong");
-    }
-  }, 1000);
-});
+// new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     console.log("Async task 2");
+//     resolve();
+//   }, 1000);
+// }).then(function () {
+//   console.log("Async 2 resolved");
+// });
 
-promiseFour
-  .then((user) => {
-    console.log(user);
-    return user.username;
-  })
-  .then((username) => {
-    console.log(username);
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
-  .finally(() => console.log("The promise is either resolved or rejected"));
+// const promiseThree = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     resolve({ username: "Chai", email: "chai@example.com" });
+//   }, 1000);
+// });
 
-const promiseFive = new Promise(function (resolve, reject) {
-  setTimeout(function () {
-    let error = true;
-    if (!error) {
-      resolve({ username: "javascript", password: "123" });
-    } else {
-      reject("ERROR: JS went wrong");
-    }
-  }, 1000);
-});
+// promiseThree.then(function (user) {
+//   console.log(user);
+// });
 
-async function consumePromiseFive() {
-  try {
-    const response = await promiseFive;
-    console.log(response);
-  } catch (error) {
-    console.log(error);
-  }
-}
+// const promiseFour = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     let error = true;
+//     if (!error) {
+//       resolve({ username: "hitesh", password: "123" });
+//     } else {
+//       reject("ERROR: Something went wrong");
+//     }
+//   }, 1000);
+// });
 
-consumePromiseFive();
+// promiseFour
+//   .then((user) => {
+//     console.log(user);
+//     return user.username;
+//   })
+//   .then((username) => {
+//     console.log(username);
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
+//   .finally(() => console.log("The promise is either resolved or rejected"));
+
+// const promiseFive = new Promise(function (resolve, reject) {
+//   setTimeout(function () {
+//     let error = true;
+//     if (!error) {
+//       resolve({ username: "javascript", password: "123" });
+//     } else {
+//       reject("ERROR: JS went wrong");
+//     }
+//   }, 1000);
+// });
+
+// async function consumePromiseFive() {
+//   try {
+//     const response = await promiseFive;
+//     console.log(response);
+//   } catch (error) {
+//     console.log(error);
+//   }
+// }
+
+// consumePromiseFive();
 
 // async function getAllUsers(){
 //     try {
@@ -89,14 +110,14 @@ consumePromiseFive();
 
 //getAllUsers()
 
-fetch("https://api.github.com/users/hiteshchoudhary")
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data);
-  })
-  .catch((error) => console.log(error));
+// fetch("https://api.github.com/users/hiteshchoudhary")
+//   .then((response) => {
+//     return response.json();
+//   })
+//   .then((data) => {
+//     console.log(data);
+//   })
+//   .catch((error) => console.log(error));
 
 // promise.all
 // yes this is also available, kuch reading aap b kro.
