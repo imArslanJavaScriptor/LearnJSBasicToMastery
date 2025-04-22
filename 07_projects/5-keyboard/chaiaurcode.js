45e6e220989252cfc3f7cbec5f69d8790ee83a77
Muhammad Arslan <imarslan7061@gmail.com>
2025-04-22 10:59:09 +0500

    Project Update
    Project 5 Keyboard Keys Tracker Completed

--- 07_projects/5-keyboard/chaiaurcode.js
+++ 07_projects/5-keyboard/chaiaurcode.js
@@ -1,1 +1,18 @@
-console.log('Project 5');
+let key = document.querySelector(".key");
+
+window.addEventListener("keydown", function (e) {
+  key.innerHTML = `
+    <table>
+    <tr>
+        <th>Key</th>
+        <th>Key Code</th> 
+        <th>Code</th>
+    </tr>
+    <tr>
+        <td>${e.key === " " ? "Space" : e.key}</td>
+        <td>${e.keyCode}</td> 
+        <td>${e.code}</td> 
+    </tr>
+    </table>
+  `;
+});
