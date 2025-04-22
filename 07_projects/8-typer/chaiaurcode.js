482fb140ef7569966ade919210750bf73872e452
Muhammad Arslan <imarslan7061@gmail.com>
2025-04-22 13:21:37 +0500

    Update
    Cursor Project Completed

--- 07_projects/8-typer/chaiaurcode.js
+++ 07_projects/8-typer/chaiaurcode.js
@@ -1,8 +1,39 @@
-const typedTextSpan = document.querySelector('.typed-text');
-const cursor = document.querySelector('.cursor');
+const typedTextSpan = document.querySelector(".typed-text")
+const cursor = document.querySelector(".cursor")
 
-const words = ['Love', 'Jhakaas', 'mast', 'dhinchak', 'Weird'];
+const words = ["Love", "Zabardast", "Dhammal", "Mast"]
 
-function type() {}
+const typingDelay = 150;
+const erasingDelay = 100;
+const newWordDelay = 1000;
 
-function erase() {}
+let wordIndex = 0
+let charIndex = 0
+
+function type() {
+    if(charIndex < words[wordIndex].length) {
+        typedTextSpan.textContent += words[wordIndex].charAt(charIndex)
+        charIndex++
+        setTimeout(type , typingDelay );
+    }else {
+        setTimeout(erase, newWordDelay);
+    }
+}
+
+
+function erase() {
+    if (charIndex > 0) {
+        charIndex--;
+        typedTextSpan.textContent = words[wordIndex].substring(0, charIndex);
+        setTimeout(erase, erasingDelay);
+    } else {
+        wordIndex++;
+        if (wordIndex >= words.length) wordIndex = 0;
+        setTimeout(type, typingDelay);
+    }
+}
+
+
+document.addEventListener("DOMContentLoaded", function() {
+    setTimeout(type, newWordDelay);
+})
